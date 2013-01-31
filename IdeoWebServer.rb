f582f7c31c59eee6dfa4818185457144d415d7c3
Francisco J Soto <fsoto@xinaptic.com>
2013-01-31 12:36:36 -0800

    disabling the ip validation

--- IdeoWebServer.rb
+++ IdeoWebServer.rb
@@ -48,13 +48,13 @@ class IdeoWebServer
       request.logger
     end
       
-    def validip(ip)
-      #uri = URI::HTTP.build(:scheme=> 'http', :host=> 'geoip.maxmind.com',
-      #      :path   => '/a', :query=> URI.encode_www_form(:l=> "S85zvqxU2ez8", :i=> ip))
-      #response = Net::HTTP.get_response(uri)
-      #response.body.encode('utf-8', 'iso-8859-1')!="TW" ? true : false
-      true
-    end
+    #def validip(ip)
+    #  #uri = URI::HTTP.build(:scheme=> 'http', :host=> 'geoip.maxmind.com',
+    #  #      :path   => '/a', :query=> URI.encode_www_form(:l=> "S85zvqxU2ez8", :i=> ip))
+    #  #response = Net::HTTP.get_response(uri)
+    #  #response.body.encode('utf-8', 'iso-8859-1')!="TW" ? true : false
+    #  true
+    #end
     
   end
   
@@ -81,33 +81,36 @@ class IdeoWebServer
   
   get '/cometopapa/?' do
     logger.info "QUERY-ING: #{params["term"]} |by| #{request.ip}"
-    pp= if validip(request.ip)
-      JSON.generate(Pair.retrieve_pairs_by_chinese(params["term"], params["lang"]))
-    else
-      nil
-    end
+    #pp= if validip(request.ip)
+    #  JSON.generate(Pair.retrieve_pairs_by_chinese(params["term"], params["lang"]))
+    #else
+    #  nil
+    #end
+    pp= JSON.generate(Pair.retrieve_pairs_by_chinese(params["term"], params["lang"]))
     response['Access-Control-Allow-Origin'] = '*'
     return pp
   end
   
   get '/fuzzy/?' do
     logger.info "QUERY-ING: #{params["term"]} |by| #{request.ip}"
-    pp= if validip(request.ip)
-      JSON.generate(Chinese.fuzzysearch(params["term"]))
-    else
-      nil
-    end
+    #pp= if validip(request.ip)
+    #  JSON.generate(Chinese.fuzzysearch(params["term"]))
+    #else
+    #  nil
+    #end
+    pp= JSON.generate(Chinese.fuzzysearch(params["term"]))
     response['Access-Control-Allow-Origin'] = '*'
     return pp
   end
   
   get '/deconstruct/?' do
     logger.info "QUERY-ING: #{params["term"]} |by| #{request.ip}"
-    pp= if validip(request.ip)
-      JSON.generate(Pair.deconstruct_chinese(params["term"]))
-    else
-      nil
-    end
+    #pp= if validip(request.ip)
+    #  JSON.generate(Pair.deconstruct_chinese(params["term"]))
+    #else
+    #  nil
+    #end
+    pp= JSON.generate(Pair.deconstruct_chinese(params["term"]))
     response['Access-Control-Allow-Origin'] = '*'
     return pp
   end
